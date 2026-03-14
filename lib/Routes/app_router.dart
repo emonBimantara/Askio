2a04bfbbda99618b45745fe39b79ca025a9151f2
emonBimantara <shelmondbimantara@gmail.com>
2026-03-14 11:15:49 +0700

    Route Setup

---
+++ lib/Routes/app_router.dart
@@ -0,0 +1,28 @@
+import 'package:askio/Features/Auth/Views/login_page.dart';
+import 'package:askio/Features/Auth/Views/register_page.dart';
+import 'package:askio/Features/Home/Views/home_page.dart';
+import 'package:askio/Features/Start/onboarding_page.dart';
+import 'package:askio/Features/Start/splash_page.dart';
+import 'package:askio/Routes/app_routes.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.splashPage:
+        return MaterialPageRoute(builder: (_) => SplashPage());
+      case AppRoutes.onboardingPage:
+        return MaterialPageRoute(builder: (_) => OnboardingPage());
+      case AppRoutes.loginPage:
+        return MaterialPageRoute(builder: (_) => LoginPage());
+      case AppRoutes.registerPage:
+        return MaterialPageRoute(builder: (_) => RegisterPage());
+      case AppRoutes.homePage:
+        return MaterialPageRoute(builder: (_) => HomePage());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(body: Center(child: Text("Page Not Found"))),
+        );
+    }
+  }
+}
---
+++ lib/Routes/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const String splashPage = "/splash";
+  static const String onboardingPage = "/onboarding";
+  static const String loginPage = "/login";
+  static const String registerPage = "register";
+  static const String homePage = "/home";
+}
